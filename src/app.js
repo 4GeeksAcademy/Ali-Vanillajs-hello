a966494d665497912a03b5e72c7d776aab8ba48e
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-06-01 08:26:42 +0000

    modificacion

--- src/app.js
+++ src/app.js
@@ -10,11 +10,12 @@ window.onload = () => {
 };
 
 let generateExcuse = () => {
-  let who = ['Carlos ', 'The dog ', 'Destiny ', 'The government '];
-  let action = ['destroyed my ', 'peed on my ', 'ate my ', 'kidnapped my '];
-  let what = ['car.', 'homework.', 'laptop.', 'life.'];
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
   let randomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-  return randomElement(who) + randomElement(action) + randomElement(what);
+  return randomElement(who) + randomElement(action) + randomElement(what) + randomElement(when);
 };
